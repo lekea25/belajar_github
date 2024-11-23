970dab5840b33931c1a5bb019934af739037228b
lekea25 <lekeareal@gmail.com>
2024-11-23 14:04:18 +0700

    belajar

---
+++ main.py
@@ -0,0 +1,38 @@
+#No 4
+#fruits = ("banana", "apple", "cherry")
+#fruits_list = list(fruits)
+#fruits_list.sort()
+#banana = [fruits_list[fruits_list.index("banana")]]
+#print(fruits_list)
+#banana_chars = list(fruits_list[1])
+#print(banana_chars) #output: ['b', 'a', 'n', 'a', 'n', 'a']'''
+
+#No 5
+#setA = {1, 2, 3, 4}
+#setB = {3, 4, 5, 6}
+#def intersection_sets(setA, setB):'''
+#return setA.intersection(setB)'''  
+#hasil = intersection_sets(setA, setB)
+#print(f"Set A: {setA}")
+#print(f"Set B: {setB}")
+#print(f"Intersection: {hasil}")'''
+
+#No 3
+
+
+#No 1
+'''Bagian dari program yang memiliki nama tertentu,''' 
+'''digunakan untuk mengerjakan suatu pekerjaan tertentu,''' 
+'''serta letaknya dipisahkan dari bagian program yang menggunakan fungsi tersebut.'''
+
+#No 2
+'''If dan Else digunakan untuk percabangan.'''
+'''Elif digunakan ketika pernyataan if pertama tidak benar, tetapi anda ingin memeriksa kondisi lain.'''
+
+
+
+
+
+
+
+
